0efad4c6e1dd8c8be8e21a03dbe3da2188e93369
Chavez Harris <codedbychavez@gmail>
2022-10-05 09:03:14 -0300

    Added feature flags and updated styling

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+import configcatclient
 
 app = Flask(__name__)
 
+configcat_client = configcatclient.create_client('YOUR_CONFIGCAT_SDK_KEY')
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    canShowCopiesSold = configcat_client.get_value('canshowcopiessold', False)
+    return render_template('index.html', canShowCopiesSold=canShowCopiesSold)
