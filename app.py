35576bfafa65b4730fb4a9c3365a5868519ae143
Chavez Harris <codedbychavez@gmail>
2022-10-06 15:36:11 -0300

    Installed amplitude and updated app

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import configcatclient
+
+from ampli import *
+
+
+ampli.load(LoadOptions(
+  environment=Environment.PRODUCTION
+))
 
 app = Flask(__name__)
 
 configcat_client = configcatclient.create_client('YOUR_CONFIGCAT_SDK_KEY')
 
-@app.route("/")
+@app.route("/", methods=('GET', 'POST'))
 def index():
     canShowCopiesSold = configcat_client.get_value('canshowcopiessold', False)
+
+    if request.method == 'POST':
+        ampli.book_purchase(user_id='user@exampled.com')
+
     return render_template('index.html', canShowCopiesSold=canShowCopiesSold)
